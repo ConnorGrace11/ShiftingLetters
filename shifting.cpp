6fb332afe3333e25d66f90c688beef1c08b8a88d
Connor Grace <0416cjg1999@gmail.com>
2023-02-25 14:46:22 -0500

    Successfully added user input , so that users can shift their own string by the amount they want.

--- shifting.cpp
+++ shifting.cpp
@@ -1,59 +1,84 @@
 #include <iostream>
 #include <vector>
-#include <map>
+
 
 using namespace std;
 
 //vector<int>& change
 void shifting(string s) {
-	int temp = 0;
-	string final = "";
-	map <char, int> letters = { {'a', 1},
-							   {'b', 2},
-							   {'c', 3},
-							   {'d', 4},
-							   {'e', 5},
-							   {'f', 6},
-							   {'g', 7},
-							   {'h', 8},
-							   {'i', 9},
-							   {'j', 10},
-							   {'k', 11},
-							   {'l', 12},
-							   {'m', 13},
-							   {'n', 14},
-							   {'o', 15},
-							   {'p', 16},
-							   {'q', 17},
-							   {'r', 18},
-							   {'s', 19},
-							   {'t', 20},
-							   {'u', 21},
-							   {'v', 22},
-							   {'w', 23},
-							   {'x', 24},
-							   {'y', 25},
-							   {'z', 26},
-	};
-	map<char, int>::iterator it;
-	char a;
-	for (int i = 0; i < s.length(); i++) {
-		cout << s[i] << endl;
-	}
-	for (auto it = letters.begin(); it != letters.end(); ++it)
-	{
-		cout << it->first << '\t' << it->second << endl;
+	int sLen = s.length();
+	vector<int> shift;
+	int num;
+	cout << "please input " << sLen << " integers greater than 0 and less than 25" << endl;
+	//get user input to determine how much they would like to shift each letter by.
+	//if it is not a number or if it shifts more than 25 times or less than 0 times, then the user must try again with the proper inputs
+	try {
+		for (int i = 0; i < sLen; i++) {
+
+			cin >> num;
+			if (num > 0 && num < 26 && isdigit(num) == false) {
+				shift.push_back(num);
+			}
+			else {
+				throw 505;
+			}
+		}
+		string final = "";
+		char a;
+
+		//print each character in the original string input
 		for (int i = 0; i < s.length(); i++) {
-			if (s[i] == it->first) {
-				a = s[i];
-				final = final + char(a+1);
+			cout << s[i] << endl;
+		}
+		
+		// shift each character in the string using their ascii values, and if they go past z, they wrap back around to a
+		for (int i = 0; i < s.length(); i++) {
+
+			a = s[i];
+			int finChar = int(a + shift[i]);
+			char adjusted = char(a + shift[i]);
+			int exceedsBy;
+			if (finChar > int('z')) {
+				exceedsBy = finChar - int('z');
+				adjusted = char('a' + exceedsBy);
+			}
+			final = final + char(adjusted);
+
+
+
+
+		}
+
+		cout << final << endl;
+	}
+	catch (...) {
+		cout << "Make sure you are selecting an INTEGER between 0 and 25!" << endl;
+	}
 				
+}
+
+//method to allow the user to input the string they want shifted, as well as making sure that they are only creating strings with letters
+string inputString() {
+	string original;
+	cout << "Please input a string that you would like to shift!" << endl;
+	try {
+		cin >> original;
+		for (int i = 0; i < original.length(); i++) {
+			if (isalpha(original[i])) {
+				continue;
+			}
+			else {
+				throw 505;
 			}
 		}
 	}
-	cout << final << endl;
+	catch (...) {
+		cout << "Please make sure that you are only selecting letters!" << endl;
+	}
+	return original;
 }
 
 int main() {
-	shifting("connor");
+	
+	shifting(inputString());
 }
